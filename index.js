147fcab0a7435a47d4881c12b085724b7fe9fe16
alessandromanzini <manzini40@outlook.it>
2019-05-22 10:25:59 +0200

    starting code

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/vox-connection", function(req, res) {
+  var temp = {
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: "Chiamata iniziata con successo!"
+            }
+          }
+        ]
+      }
+    }
+  };
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
+      : "Non credo di aver capito bene.";
+  return res.json({
+    payload: temp,
+    data: temp,
+    fulfillmentText: "vox-connection",
+    speech: speech,
+    displayText: speech,
+    source: "webhook-voxdf-connection"
+  });
+});
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
